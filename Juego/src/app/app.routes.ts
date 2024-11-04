221c9fc801ac96d310b393a66505afa7af290e86
Pablo900576 <rjc61662@educastur.es>
2024-11-04 19:49:31 +0100

    Pepe

--- Juego/src/app/app.routes.ts
+++ Juego/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AutoresComponent } from './menu/autores/autores.component';
+import { ReglasComponent } from './menu/reglas/reglas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:ReglasComponent}
+];
