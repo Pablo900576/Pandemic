4b9f13e9a470dc9ee267b7a2e82e3d664e99fe53
miguelrg16 <145588049+miguelrg16@users.noreply.github.com>
2024-11-05 16:57:24 +0100

    siisi

--- Juego/src/app/menu/menu-routing.module.ts
+++ Juego/src/app/menu/menu-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuJuegoComponent } from './menu-juego/menu-juego.component';
 import { ReglasComponent } from './reglas/reglas.component';
+import { AutoresComponent } from './autores/autores.component';
 
 const routes: Routes = [
     { path: '', component: MenuJuegoComponent },
-    {path:'reglas', component: ReglasComponent}
+    {path:'reglas', component: ReglasComponent},
+    {path:'autores', component: AutoresComponent},
 ];
 
 
