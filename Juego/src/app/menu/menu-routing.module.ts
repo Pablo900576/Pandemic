e262c139c70aea3d5d1a700af4776b08f2a85578
Pablo900576 <rjc61662@educastur.es>
2024-11-05 16:57:10 +0100

    pepe

--- Juego/src/app/menu/menu-routing.module.ts
+++ Juego/src/app/menu/menu-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuJuegoComponent } from './menu-juego/menu-juego.component';
-import { ReglasComponent } from './reglas/reglas.component';
+import { ReglasComponent } from './menu-informacion/reglas/reglas.component';
+import { AutoresComponent } from './menu-informacion/autores/autores.component';
 
 const routes: Routes = [
     { path: '', component: MenuJuegoComponent },
-    {path:'reglas', component: ReglasComponent}
+    {path:'reglas', component: ReglasComponent},
+    {path:'regla1', component: AutoresComponent},
+    {path:'menu', component: MenuJuegoComponent}
 ];
 
 
