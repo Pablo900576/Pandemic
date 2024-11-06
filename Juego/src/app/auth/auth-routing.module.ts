746e591686616106ea8a40526b631c5e3b562460
miguelrg16 <145588049+miguelrg16@users.noreply.github.com>
2024-11-06 21:07:02 +0100

    soy una locura

--- Juego/src/app/auth/auth-routing.module.ts
+++ Juego/src/app/auth/auth-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'', component:RegisterComponent}
+  {path:'', component: RegisterComponent},
+  {path:'menu', loadChildren: () => import('../menu/menu.module').then(m => m.MenuModule) },
 ];
 
 @NgModule({
--- Juego/src/app/auth/register/register.component.ts
+++ Juego/src/app/auth/register/register.component.ts
@@ -1,22 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,ReactiveFormsModule, CommonModule ],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
+
 export class RegisterComponent {
   miFormulario= new FormGroup({
-    nombre: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    mensaje: new FormControl('', [Validators.required])
+    contraseña: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
   });
 
   enviar(){
